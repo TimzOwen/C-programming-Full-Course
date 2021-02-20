b51c6da48d0dc50d3801c75c9aa73381e3b147e2
Timz Owen <ktimothy@kabarak.ac.ke>
2021-02-20 14:31:41 +0300

    Create chapter 1.cpp
    variables update

---
+++ Introduction/chapter 1.cpp
@@ -0,0 +1,26 @@
+# include<iostream>
+
+using namespace std;
+
+int main(){
+ 	// data Types
+ 	
+ 	//char --> used to store single character in double/single quotes
+ 	char myChar = "a";
+ 	
+ 	// String --> Used to store a sequences of characters in a variable name
+ 	string myCommunity = "Dsc Kabarak university"<<endl;
+ 	
+ 	// integer --> used to store whole numbers without decimal points
+ 	int myAge = 25;
+ 	
+ 	// float --> stores less decimal points than double
+ 	float myShare = 5.50;
+ 	
+ 	// double --> used for storing numbers with decimal numbers BUT with more number of decimal places than floa
+ 	double myRate = 45.7805985
+ 	
+ 	//boolean -->
+ 
+	return 0;
+}
