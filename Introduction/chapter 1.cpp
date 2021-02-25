4057fa9d7cf955a4bb49c03e27743a8c55172de1
Timz Owen <ktimothy@kabarak.ac.ke>
2021-02-25 16:38:40 +0300

    Update chapter 1.cpp
    project architure of C

--- Introduction/chapter 1.cpp
+++ Introduction/chapter 1.cpp
@@ -1,7 +1,14 @@
-# include<iostream>
+
+// configuration of the project
+#include<iostream>
 
 using namespace std;
 
+// programming--> giving your computer a set of instructions to execute;
+
+//This is the main function that gets called before any code gets executed
 int main(){
- 
-}
+    // print Hello world of programming;
+	cout<<"Hello world" <<endl;
+    cout<<"Hello owen "<< endl;
+	return 0;
