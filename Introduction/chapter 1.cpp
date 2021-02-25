20a98051a5b062714f0bcfa6b8b72388871be81a
Timz Owen <ktimothy@kabarak.ac.ke>
2021-02-25 16:42:14 +0300

    Update chapter 1.cpp
    updated printing to ouput

--- Introduction/chapter 1.cpp
+++ Introduction/chapter 1.cpp
@@ -9,6 +9,34 @@ using namespace std;
 //This is the main function that gets called before any code gets executed
 int main(){
     // print Hello world of programming;
-	cout<<"Hello world" <<endl;
+    cout<<"Hello world" <<endl;
     cout<<"Hello owen "<< endl;
-	return 0;
+   
+    
+    // printing Right angle Triangle
+    // order of execution
+    cout<<"			" <<endl;
+	cout<<"		 /| " <<endl;
+	cout<<"	    / | " <<endl;
+	cout<<"    /  | " <<endl;
+	cout<<"   /   | " <<endl;
+	cout<<"  /    | " <<endl;
+	cout<<" /_____| " <<endl;
+
+    // Variables;
+    // container for storing different information in a computer memory
+    // booking space    
+    string myName = "Owen";
+    int myAge ;
+    myAge = 30;
+    cout<<"my name is "<<myName<< endl;
+    cout<<"and my age is "<<myAge<< "years"<<endl;
+
+    // updating variables and declaring and asigning variables;
+    string myName = "Timz Owen";
+ 	int myAge = 20;
+ 	string mySchool;
+ 	mySchool = "Kabarak University";
+ 	cout<<"My name is "<<myName<<endl;
+ 	cout<<"I am "<<myAge<<" years"<<endl;
+ 	cout<<"I school at "<<mySchool<< " in Nakuru Kenya."<<endl;
