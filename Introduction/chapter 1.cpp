5399f7b180c6f20bd47ebd25ed56ff3b45c43bd7
Timz Owen <ktimothy@kabarak.ac.ke>
2021-02-25 16:44:02 +0300

    Update chapter 1.cpp
    updated strings

--- Introduction/chapter 1.cpp
+++ Introduction/chapter 1.cpp
@@ -40,3 +40,24 @@ int main(){
  	cout<<"My name is "<<myName<<endl;
  	cout<<"I am "<<myAge<<" years"<<endl;
  	cout<<"I school at "<<mySchool<< " in Nakuru Kenya."<<endl;
+
+    
+    // DATA TYPES:
+
+    //char ---> used to represent single character in our computer memory 
+	char score = "B";
+	
+	// string is a combination of character , multiple concatenated char
+	string myName = "Hello C";
+	
+	// intergers --> this are whole numbers
+	int myAge = 40;
+	
+	// floating point --> (decimal) . represent decimal poin numbers (float and double->stores more decimal points than flaot)
+	double bankBalance = 59.35898723795738;
+	float rate = 59.90;
+	
+	// Boolean --> represent two state data , True/False 0r 1/0;
+	bool isRaning = true;
+
+	
