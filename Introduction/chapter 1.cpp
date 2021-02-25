9f591528a46cb975fbe5d1519b9ebfb2cb078458
Timz Owen <ktimothy@kabarak.ac.ke>
2021-02-25 16:45:05 +0300

    Update chapter 1.cpp
    strings method update

--- Introduction/chapter 1.cpp
+++ Introduction/chapter 1.cpp
@@ -61,3 +61,43 @@ int main(){
 	bool isRaning = true;
 
 	
+     
+
+    // String s in strings
+    // Strings in C programming:
+    
+    // printing in new line (endl, \n)
+    
+    // print directly to the output string;
+	cout<<"70"<<endl;    
+	
+	// print with variables
+	string userName = "Timz Owen";
+	cout<<userName<<endl;
+	
+	
+	
+	// String methods in C programming 
+	// just like functions this uses a block of code designed to do a predefined tasks, Houses multiple reusable code 
+	cout<<userName.length()<<endl;  // prints 9 characters including space
+	
+	//indexing strings
+	cout<<userName[0]<<endl;  // accesing first element 
+	
+	// update string with index  use single and not double quotes
+	userName[0] = 'O';
+	cout<<userName<<endl;
+	
+	// Find elements existance in a string
+	cout<<userName.find("Owen",0)<<endl;	 // returns the first occurance of the word/ string
+	
+	// creating new string wit index
+	cout<<userName.substr(0,4)<<endl;  // the parameter take in the starting and end index;
+	
+	string newName = userName.substr(4,5);
+	cout<<newName<<endl;
+	
+
+
+    // NUMBERS IN C PROGRAMMING 
+	
