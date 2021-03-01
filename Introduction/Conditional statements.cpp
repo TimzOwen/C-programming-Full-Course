a014100767ab7cde16469606d05f38e2e95e3e32
Timz Owen <ktimothy@kabarak.ac.ke>
2021-03-01 18:01:26 +0300

    Update Conditional statements.cpp
    comparison and conditional statements

--- Introduction/Conditional statements.cpp
+++ Introduction/Conditional statements.cpp
@@ -57,3 +57,56 @@
 	else{
 		cout<<"You failed";
 	}
+
+	
+    //restructure the code to handle scores more than 100% score and not less than 0; combine operators:
+
+    int score = 90;
+	int passMark = 80;
+	
+	if (score == passMark){
+		cout<<"You on the pass mark line "<<passMark<<endl;
+	}else if(score>passMark && score<=100){
+		cout<<"You are  Genius, passed passMark"<<endl;
+	}
+	else if(score>=0 && score <passMark){
+		cout<<"You failed";
+	}
+	else{
+		cout<<"Incorrect input, enter number between 0-100";
+	}
+
+    // task:
+    // if raining-->wear jacket, if sunshine--> go swimming --else watch a movie:(use boolean values--> True/False)
+    // isRaning, !isRaining:
+
+    // Use comparison  operator to find largets of 2 numbers;
+    int num1 = 10;
+	int num2 = 30;
+	int largest; // This variable is used to store the largets value
+	if(num2>num1){
+		largest =  num2;	
+	}
+	else{
+		largest = num1;
+	}
+	cout<<largest;
+
+
+    // Top scorer of three awards Championship:
+    // Tasks: use comparison operators
+    int team1 = 20;
+    int team2 = 40;
+    int team3 = 15;
+    int winningScore;
+
+    if (team1>team2 && team1>team3){
+        winningScore = team1;
+    }else if (team2>team1 && team2>team3){
+        winningScore = team2;
+    }else{
+        winningScore = team3;
+    }
+    cout<<"Winning Score is: "<<winningScore;
+
+
